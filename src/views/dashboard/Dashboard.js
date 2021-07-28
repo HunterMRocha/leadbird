676155ec413373613eec2bc96e8d70c859a2d528
HunterMRocha <huntermacias20@gmail.com>
2021-07-28 10:18:54 -0700

    added message filter

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -10,10 +10,10 @@ import {
   CCardBody,
   CCardFooter,
   CCol,
-  CForm,
   CFormControl,
   CInputGroup,
   CProgress,
+  CPopover,
   CRow,
   CTableDataCell,
   CTableHeaderCell,
@@ -32,13 +32,21 @@ const Dashboard = () => {
   const { account, campaign } = queryString.parse(search);
 
   const [data, setData] = useState([]);
-  const [q, setQ] = useState("");
+  const [FNQuery, setFNData] = useState("");
   const [totalMessaged, setMess] = useState();
   const [totalRequests, setTR] = useState();
   const [totalAccepted, setTA] = useState();
   const [successPerc, setSP] = useState();
   const [responseRate, setRR] = useState();
+  const [filterVal, setVal] = useState();
+  
+  const filterByRecentMessages = () => {
+    setVal(1);
+  }
 
+  const filterByRecentConnections = () => {
+    setVal(0);
+  }
 
 
   //GET Request function to API
@@ -46,12 +54,13 @@ const Dashboard = () => {
     fetch(`https://api.apispreadsheets.com/data/${account}/`)
       .then( res => {
         res.json().then(data => {
+          setVal(1);
           setData(data.data);
           setMess(data.data[0].TotalMessaged);
           setTR(data.data[0].TotalRequests);
           setTA(data.data[0].TotalConnections);
-          setSP(((data.data[0].TotalConnections / data.data[0].TotalRequests) * 100.0).toFixed(2))
-          setRR(((data.data[0].TotalMessaged / data.data[0].TotalConnections) * 100).toFixed(2))
+          setSP(((data.data[0].TotalConnections / data.data[0].TotalRequests) * 100.0).toFixed(2));
+          setRR(((data.data[0].TotalMessaged / data.data[0].TotalConnections) * 100).toFixed(2));
         })    
     })
   }
@@ -62,7 +71,11 @@ const Dashboard = () => {
   }, []);
 
   function searchTable(rows){
-    return rows.filter(row => row.FirstName.toLowerCase().indexOf(q) > -1)
+    if (filterVal === 1){
+      return rows.sort((a,b) => new Date(b.MessageTime) - new Date(a.MessageTime));
+    }
+    return rows.filter(row => row.FirstName.toLowerCase().indexOf(FNQuery) > -1);
+
   }
 
 
@@ -70,37 +83,38 @@ const Dashboard = () => {
     <>
       <CCard className="lg-5">
         <CCardFooter>
+      
           <CRow className="text-center">
             <CCol lg md sm="12" className="lg-mb-sm-2 mb-0">
-              <CButton className="button-format" color="dark" variant="outline" size="lg">
+              <CButton className="button-format" color="dark" variant="outline" size="lg" onClick={filterByRecentConnections}>
                 <div className="text-large-emphasis">Connection Requests</div>
                 <strong>{totalRequests} Requests</strong>
                 <CProgress thin className="mt-2" precision={1} color="success" value={100} />
               </CButton>
             </CCol>
             <CCol lg md sm="12" className="lg-mb-sm-2 mb-0">
-              <CButton className="button-format" color="dark" variant="outline" size="lg">
+              <CButton className="button-format" color="dark" variant="outline" size="lg" onClick={filterByRecentConnections}>
                 <div className="text-medium-emphasis">Connections Accepted</div>
                 <strong>{totalAccepted} Accepted</strong>
                 <CProgress thin className="mt-2" precision={1} color="info" value={successPerc} />
               </CButton>
             </CCol>
             <CCol lg md sm="12" className="lg-mb-sm-2 mb-0">
-              <CButton className="button-format"  color="dark" variant="outline" size="lg">
+              <CButton className="button-format"  color="dark" variant="outline" size="lg" onClick={filterByRecentConnections}>
                 <div className="text-medium-emphasis">Connection Success Rate</div>
                 <strong>{successPerc}%</strong>
                 <CProgress thin className="mt-2" precision={1} color="warning" value={successPerc} />
               </CButton>
             </CCol>
             <CCol lg md sm="12" className="lg-mb-sm-2 mb-0">
-              <CButton className="button-format" color="dark" variant="outline" size="lg">
+              <CButton className="button-format" color="dark" variant="outline" size="lg" onClick={filterByRecentMessages}>
                 <div className="text-medium-emphasis">Messages Returned</div>
                 <strong>{totalMessaged} Messaged</strong>
                 <CProgress thin className="mt-2" precision={1} color="danger" value={totalMessaged} />
               </CButton>
             </CCol>
             <CCol lg md sm="12" className="lg-mb-sm-2 mb-0">
-              <CButton className="button-format"  color="dark" variant="outline" size="lg">
+              <CButton className="button-format"  color="dark" variant="outline" size="lg" onClick={filterByRecentMessages}>
                 <div className="text-medium-emphasis">Message Response Rate</div>
                 <strong>{responseRate}%</strong>
                 <CProgress thin className="mt-2" precision={1} value={responseRate} />
@@ -118,16 +132,14 @@ const Dashboard = () => {
           value={"campaign title: " + campaign}
           size="lg"
         />
-        {/* <CButton type="button" color="primary" id="button-addon2">
-          <strong>Download Data</strong>
-        </CButton> */}
+ 
       </CInputGroup>
       
       <CRow>
         <CCol xs>
           <CCard className="mb-4">
             <CInputGroup className="mb-3">
-              <CFormControl type="text" placeholder="Search By First Name: " value={q} onChange={(e) => setQ(e.target.value)} />
+              <CFormControl type="text" placeholder="Search By First Name: " value={FNQuery} onChange={(e) => setFNData(e.target.value)} />
             </CInputGroup>
             <CCardBody>
               <MDBTable scrollY maxHeight="500px" hover responsive align="middle" className="mb-0 border">
@@ -147,20 +159,23 @@ const Dashboard = () => {
                 <MDBTableBody>
                   {
                     Object.values(searchTable(data.slice(1, data.length))).map((item) => (
-                     
                       <CTableRow className="text-left">
-                        <CAvatar className="col-xl col-lg col-md col-sm" size = "xl" src={item.Picture} />
+                        <CAvatar className="col-xl col-lg col-md col-sm" size="xl" src={item.Picture}><CIcon size="xl" name="cil-user"></CIcon> </CAvatar> 
                         <CTableDataCell>
-                          <div className="user-info">
+                          <div className="user-info font-style">
                             <strong>Name:</strong>  {item.FirstName} {item.LastName}
                           </div>
-                          <div className="user-info">
+                          <div className="user-info font-style">
                             <span>
                               <strong>Company:</strong> {item.CompanyName}
                             </span>
                             <br />
                             <span className="fixed-width"> 
                               <strong>Title:</strong> {item.LeadTitle}
+                            </span>
+                            <br />
+                            <span>
+                              <strong>Email: </strong> {item.Email}
                             </span>
                             <br />
                             <span>
@@ -172,26 +187,18 @@ const Dashboard = () => {
                           <MDBInput type="checkbox" checked={item.Connected} />
                         </CTableDataCell>
                         <CTableDataCell> 
-                          <a target="_blank" href={item.ProfileLink}>
+                          <a target="_blank" rel="noreferrer" href={item.ProfileLink}>
                             <CIcon size="xl" name="cib-linkedin" />
                           </a>
                         </CTableDataCell>
                         <CTableDataCell>
-                          <a target="_blank" href={item.MessageURL}>
+                          <a target="_blank" rel="noreferrer" href={item.MessageURL}>
                             <CIcon size="xl" name="cil-comment-square" />
                           </a>
                         </CTableDataCell>
                         <CTableDataCell>
-                          <CForm>
-                            <div className="box-width">
-                              <CFormControl
-                                component="textarea"
-                                id="exampleFormControlTextarea1"
-                                rows="5"
-                                placeholder="No Message Available"
-                              >{item.MessageResponse}</CFormControl>
-                            </div>
-                          </CForm>
+                          <MDBInput className="box-width" type="textarea"  rows="4" value={item.MessageResponse}>  
+                          </MDBInput>
                         </CTableDataCell>
                       </CTableRow>
                     ))
